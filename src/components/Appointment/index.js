4ac9adb9b6d432a01e98ca02638a69e4f4b7414c
Jacob1225 <carlone.jacob@gmail.com>
2020-05-16 20:43:26 -0400

    added the useVisualMode into the appointment component to allow different child components to display

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,16 +2,45 @@ import React, { Fragment } from 'react'
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
+      );
+
+    const handleOnAdd = () => {
+        transition(CREATE);
+    }
+
+    const handleCancel = () => {
+        back();
+    }
+
     return (
         <article className="appointment">
             <Header 
             time= {props.time}/>
-            {props.interview ? <Show student= {props.interview.student} 
-               name ={props.interview.interviewer.name}/> : <Empty />}
+            {mode === EMPTY && (<Empty  onAdd={handleOnAdd} />)}
+            {mode === SHOW && (
+                <Show
+                    student={props.interview.student}
+                    name={props.interview.interviewer.name}
+                />
+            )}
+            {mode === CREATE && (<Form
+                name={props.name}
+                value={props.value}
+                interviewers={[]}
+                onSave={props.onSave}
+                onCancel={handleCancel} /> )}
+            
         </article>
     )
 };
